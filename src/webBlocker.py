c1d07697f70af763435b196b395b8811fa23d6d2
wonwoo.song <songw@unbc.ca>
2020-03-04 22:56:56 -0800

    initial commit for the project

---
+++ src/webBlocker.py
@@ -0,0 +1,10 @@
+import time
+from datetime import datetime as dtime
+
+hosts_path = r"hosts"
+redirect="127.0.0.1"
+website_list=["www.facebook.com","facebook.com"]
+
+while True:
+    print(1)
+    time.sleep(10)
